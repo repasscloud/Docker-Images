0e7e22106be10baad835762fdcdd808bce872cec
danijeljw-rpc <danijel@repasscloud.com>
2022-11-29 23:52:16 +1100

    update process

--- ubuntu-20.04/apache/build.sh
+++ ubuntu-20.04/apache/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-#SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker build --rm --no-cache --tag repasscloud/ubuntu-20.04:apache .
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+docker build --rm --no-cache --tag repasscloud/ubuntu-20.04:apache $SCRIPT_DIR
